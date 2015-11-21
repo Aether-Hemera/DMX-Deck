725b811aefa890c52f2c7407e5ad6388586a0718
Claudio Benghi <claudio.benghi@gmail.com>
2015-11-21 15:57:35 +0000

    Class renaming.

--- Voyage Deck/SkeletonWinforms/Program.cs
+++ Voyage Deck/SkeletonWinforms/Program.cs
@@ -15,7 +15,7 @@ namespace SkeletonWinforms
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmMain());
         }
     }
 }
--- Voyage Deck/SkeletonWinforms/frmMain.cs
+++ Voyage Deck/SkeletonWinforms/frmMain.cs
@@ -15,9 +15,9 @@ using SkeletonWinforms.Properties;
 
 namespace SkeletonWinforms
 {
-    public partial class Form1 : Form
+    public partial class frmMain : Form
     {
-        public Form1()
+        public frmMain()
         {
             InitializeComponent();
             chkSerialStart.Checked = Settings.Default.SerialStart;
